d627af07be552b74f5f908f7720baef00792a887
JulianaMarques <julianabmarques@live.com>
2017-12-19 01:07:42 -0300

    Update do arquivo ListaDuplamenteEncadeada.cpp

--- ListaDuplamenteEncadeada/ListaDuplamenteEncadeada.cpp
+++ ListaDuplamenteEncadeada/ListaDuplamenteEncadeada.cpp
@@ -35,7 +35,7 @@ class Lista {
 			void inserir(int valor) {
 				No *no = new No(valor);
 
-				// ATRIBUI VALOR A LISTA
+				// ATRIBUI VALOR A LISTA (NÓ APONTA PARA VALOR)
 				no -> valor = valor;
         
    	   	   	   // CRIA A LISTA
@@ -54,10 +54,11 @@ class Lista {
    	   	   	   no -> prox = NULL;
    	   	   	   no -> ant = fim;
    	   	   	   fim = no;
-   
+   	   	   	   
    	   	   	   return ;
 			}
-					
+			
+			// RECEBE VALOR E POSIÇÃO COMO PARÂMETRO
 			void inserirNoMeio(int valor, int i) {
 				No *no = new No(valor);
 				No *atual = inicio;
@@ -70,36 +71,83 @@ class Lista {
 						no -> prox = atual; // PONTEIRO PRÓXIMO DO NÓ RECEBE ATUAL 
 						no -> ant = atual -> ant; // PONTEIRO ANTERIOR DO PRÓXIMO NÓ RECEBE PONTEIRO ANTERIOR DO NÓ ATUAL
 						atual -> ant -> prox = no; // PONTEIROS ATUAL, ANTERIOR E PRÓXIMO APONTAM PARA O PRÓXIMO NÓ
-						atual -> ant = no; // ATUAL PASSA PRA PRÓXIMA POSIÇÃAO E SEU PONTEIRO ANTERIOR APONTA PARA O NÓ
+						atual -> ant = no; // ATUAL PASSA PRA PRÓXIMA POSIÇÃO E SEU PONTEIRO ANTERIOR APONTA PARA O NÓ
 						
 						return ;
 					}
 					
-					atual = atual->prox;
+					atual = atual -> prox; // ATUAL PASSA PARA O PRÓXIMO VALOR
 					indice++;
 				}
+			}
+       
+       		 // RECEBE UM VALOR E ADICIONA NO MEIO DA LISTA (COM BASE NA MEDIANA)
+       		 void inserirNoMeio(int valor) {
+ 		 		No *no = new No(valor);
+				No *atual = inicio;
+				
+				int indice = 0;
+				int tamanho = 0;
+				float mediana = 0.0;
+				
+				// PERCORRE A LISTA CONTA AS POSIÇÕES
+				while(atual != NULL) {
+					atual = atual -> prox;
+					tamanho++;
+				}
+				
+				atual = inicio; // SETA O ATUAL PARA A POSIÇÃO INICIAL PARA PERCORRER NOVAMENTE
+				
+				// POR ALGUM MOTIVO A MEDIANA TÁ SENDO ARREDONDADA PARA BAIXO, MAS NÃO TEM PROBLEMA
+				mediana = tamanho / 2;
+				
+				// PERCORRE NOVAMENTE E COMPARA O VALOR DO INDICE COM A MEDIANA
+				// SE FOREM IGUAIS O NOVO NÓ É ADICIONADO NA POSIÇÃO	
+				while(atual != NULL) {
+					if(indice == mediana) {
+						no -> prox = atual; 
+						no -> ant = atual -> ant;
+						atual -> ant -> prox = no;
+						atual -> ant = no;
+						
+						break;
+					}
+					
+					atual = atual -> prox;
+					indice++;
+				}
+				
+				cout << "Mediana: " << mediana << endl;
+				cout << "Tamanho: " << tamanho << endl;
+				cout << "" << endl;
 			}
 			
 			void mostrar(){
 				No *atual = inicio;
 				
-				while (atual!=NULL){
-					cout<<"\nValor: "<<atual->valor<<endl;
-					atual=atual->prox;
+				while(atual != NULL){
+					cout << "\nValor: " << atual -> valor << endl;
+					atual = atual -> prox;
 				}
    			}
 };
 
 int main() {
 	Lista l;
+	
 	l.inserir(3);
 	l.inserir(8);
 	l.inserir(7);
 	l.inserir(9);
 	l.inserir(45);
 	l.inserir(50);
-	l.inserirNoMeio(2,5); //(VALOR, POSIÇÃO)
-	l.inserirNoMeio(99,3);
+	l.inserir(70);
+	//l.inserirNoMeio(2,5); //(VALOR, POSIÇÃO)
+	//l.inserirNoMeio(99,3);
+	l.inserirNoMeio(88);
+	l.inserirNoMeio(90);
+	
 	l.mostrar();
+	
 	return 0;
 }
