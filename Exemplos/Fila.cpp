164486cd78051fa70fad232b3753dc8159872863
JulianaMarques <julianabmarques@live.com>
2018-02-04 22:16:53 -0300

    Adição dos arquivos Fila.cpp e Pilha.cpp

---
+++ Exemplos/Fila.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class No {
+	public:
+		int valor;
+		No *prox;
+	
+		No(int v) {
+			valor = v;
+			prox = NULL;
+		}
+};
+
+class Fila{
+	public:
+		No *inicio;
+		No *fim;
+		
+		Fila(){
+			inicio = NULL;
+			fim = NULL;
+		}
+		
+		void push(int n){
+			No *no = new No(n);
+			
+			if(inicio == NULL){
+				inicio = no;
+				fim = no;
+			}
+			
+			else{
+				fim -> prox = no;
+				fim = no;
+			}
+		}
+		
+		bool estaVazia() {
+			if(inicio == NULL) {
+				return true;
+			}
+		
+			return false;
+		}
+		
+		int pop(){
+			No *temp;
+			int valor;
+			
+			if(estaVazia()){
+				temp = inicio;
+				valor = inicio -> valor;
+				inicio = inicio -> prox;
+				free(temp);
+			}
+			
+			return valor;
+		}
+	
+		void exibir(){
+			No *atual = inicio;
+			
+			while(atual != NULL){
+				cout << "End: " << &atual << " | Valor: " << atual -> valor << " | Prox: " << atual -> prox << endl;
+				atual = atual -> prox;
+			}
+		}
+};
+
+int main() {
+	Fila f;
+	
+	f.push(3);
+	f.push(2);
+	f.push(4);
+	f.push(1);
+	
+	f.exibir();
+	f.pop();
+	
+	cout << endl;
+	
+	f.exibir();
+}
---
+++ Exemplos/Pilha.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class No {
+	public:
+		int valor;
+		No *prox;
+	
+		No(int v) {
+			valor = v;
+			prox = NULL;
+		}
+};
+
+class Pilha {
+	public:
+		No *topo;
+	
+		Pilha() {
+			topo = NULL;
+		}
+	
+		bool estaVazia() {
+			if(topo == NULL) {
+				return true;
+			}
+		
+			return false;
+		}
+	
+		void push(int valor) {
+			No *no = new No(valor);
+		
+			if(estaVazia()) {
+				topo = no;
+			}
+		
+			else {
+				no -> prox = topo;
+				topo = no;
+			}
+		}
+	
+		int pop(){
+			No *temp;
+			int popped;
+		
+			if(topo != NULL){
+				temp = topo;
+				popped = temp -> valor;
+				topo = topo -> prox;
+				free(temp);
+			}
+		
+			return popped;
+		}
+		
+		// NÃO ACONSELHÁVEL PARA PILHAS	
+		void exibir(){
+			No *atual = topo;
+		
+			while(atual != NULL){
+				cout << "End: " << &atual << " | Valor: " << atual -> valor << " | Prox: " << atual -> prox << endl;
+				atual = atual -> prox;
+			}
+		}
+};
+
+int main() {
+	Pilha p;
+	
+	p.push(5);
+	p.push(3);
+	p.push(7);
+	
+	p.exibir();
+	p.pop();
+	
+	cout << endl;
+	
+	p.exibir();
+	
+	return 0;
+}
+
